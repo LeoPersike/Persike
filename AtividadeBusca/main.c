06990521831900ef59ba52648474b39e36640bd8
LeoPersike <leonardo.persike.martins@gmail.com>
2015-05-22 11:14:51 -0300

    Versao 2 da atividade de busca.

--- AtividadeBusca/main.c
+++ AtividadeBusca/main.c
@@ -32,12 +32,59 @@ struct circulo {
 typedef struct circulo Circulo;
 
 // Prototipos
+int compara_centro (const void* a,const void* b);
 
-#define N 5 // Numero de elementos nos vetores
+#define N 5 // Numero de elementos
 
 int main()
 {
     Circulo circulos[N];
+    int aux;
+    // Adicionando os circulos
+    //(40,60)
+    circulos[0].cx = 40;
+    circulos[0].cy = 60;
+    //(200,200)
+    circulos[1].cx = 200;
+    circulos[1].cy = 100;
+    //(70,100)
+    circulos[2].cx = 70;
+    circulos[2].cy = 100;
+    //(30,80)
+    circulos[3].cx = 30;
+    circulos[3].cy = 80;
+    //(150,45)
+    circulos[4].cx = 150;
+    circulos[4].cy = 45;
+
+    // Imprimindo os circulos
+    printf("\nImpressao sem ordenamento");
+    for(aux=0;aux<N;aux++)
+        printf("\n Circulo %d. Centro em (%d,%d).",aux+1,circulos[aux].cx,circulos[aux].cy);
+    // Ordenando os circulos
+    qsort(circulos,N,sizeof(Circulo),compara_centro);
+    printf("\nImpressao depois de ordenamento generico quicksort");
+    for(aux=0;aux<N;aux++)
+        printf("\n Circulo %d. Centro em (%d,%d).",aux+1,circulos[aux].cx,circulos[aux].cy);
 
     return 0;
+}
+
+
+int compara_centro (const void* a,const void* b)
+{
+    Circulo* c1 = (Circulo*) a;
+    Circulo* c2 = (Circulo*) b;
+
+    if(c1->cy < c2->cy) // Nao precisa trocar pois y1 < y2
+        return 0;
+    else if (c1->cy == c2->cy) // Se os y sao iguais verificar o x
+    {
+        if (c1->cx < c2->cx) // Nao precisa trocar pois y1 = y2 e x1 < x2
+            return 0;
+        else             // Troca pois y1 = y2 e x1 > x2
+            return 1;
+    }
+    else return 1; // Troca pois y1 > y2
+
 }
