9ad9882f8e2c076e139b484a1eac40c6b65cffbf
LeoPersike <leonardo.persike.martins@gmail.com>
2015-06-01 08:22:03 -0300

    Exercicios com arvores 12-6 (implementacao da funcao imprime com estrutura) - Exercicios baseados na apostila Celles e Rangel estrutura de dados PUC Rio

--- arvore/main.c
+++ arvore/main.c
@@ -16,47 +16,52 @@ struct arv {
     struct arv* esq;
     struct arv* dir;
 };
-typedef struct arv Arv;
+typedef struct arv Arvore;
 
 // Prototipos
-Arv* inicializa(void);
-Arv* cria(char c, Arv* sae, Arv* sad);
-int vazia(Arv* a);
-void imprime (Arv* a);
+Arvore* inicializa(void);
+Arvore* cria(char c, Arvore* sae, Arvore* sad);
+int vazia(Arvore* a);
+void imprime (Arvore* a);
+void imprime_estrutura (Arvore* a);
 
 
 int main()
 {
-    Arv* a = cria('a',
+    Arvore* a = cria('a',
                 cria('b',inicializa(),cria('d', inicializa(), inicializa())
                 ),
                 cria('c',cria('e', inicializa(), inicializa()),cria('f', inicializa(), inicializa())));
     imprime(a);
+    printf("\n");
+    imprime_estrutura(a);
     return 0;
 }
 
 /****************** Funcoes *******************/
-
-// Funcoes
-Arv* inicializa(void)
+// Funcao para inicializar uma arvore vazia
+Arvore* inicializa(void)
 {
     return NULL;
 }
 
-Arv* cria(char c, Arv* sae, Arv* sad){
-    Arv* p=(Arv*)malloc(sizeof(Arv));
+// Funcao para criar uma arvore com informacao e sub arvores
+Arvore* cria(char c, Arvore* sae, Arvore* sad){
+    Arvore* p=(Arvore*)malloc(sizeof(Arvore));
     p->info = c;
     p->esq = sae;
     p->dir = sad;
     return p;
 }
 
-int vazia(Arv* a)
+// Funcao para checar se a lista esta vazia
+int vazia(Arvore* a)
 {
     return a==NULL;
 }
 
-void imprime (Arv* a)
+// Funcao para imprimir o conteudo da arvore
+void imprime(Arvore* a)
 {
     if (!vazia(a))
     {
@@ -64,4 +69,24 @@ void imprime (Arv* a)
         imprime(a->esq);
         imprime(a->dir);
     }
+}
+
+// Funcao para imprimir a arvore com a estrutura <<>><><><><>
+void imprime_estrutura(Arvore* a)
+{
+    printf("<");
+    if (!vazia(a))
+    {
+        printf("%c",a->info);
+        if(!vazia(a->esq))
+            imprime_estrutura(a->esq);
+        else
+            printf("<>");
+
+        if(!vazia(a->dir))
+            imprime_estrutura(a->dir);
+        else
+            printf("<>");
+    }
+    printf(">");
 }
